33d98ac734dd8937f6d4b23cb828d31ba928d48e
Nataliya <nataliyakl8@icloud.com>
2023-06-04 06:26:23 -0500

    Tumblr task

--- src/test/java/Mentoring3/TumblrTask.java
+++ src/test/java/Mentoring3/TumblrTask.java
@@ -1,7 +1,9 @@
 package Mentoring3;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
@@ -31,6 +33,29 @@ public class TumblrTask {
         provide email, and password (wrong credentials)
         and validate the error message
         driver quit*/
+
+        WebElement logIn= driver.findElement(By.linkText("Log in"));
+        logIn.click();
+
+        WebElement continueWEmail= driver.findElement(By.xpath("//button[@class='dKGjO']"));
+        continueWEmail.click();
+
+        WebElement email= driver.findElement(By.xpath("//input[@type='email']"));
+        email.sendKeys("mail@mail.com");
+
+        WebElement nextButton= driver.findElement(By.xpath("//button[@aria-label='Next']"));
+        nextButton.click();
+
+        WebElement password= driver.findElement(By.xpath("//input[@type='password']"));
+        password.sendKeys("123");
+
+        WebElement logIn2=driver.findElement(By.xpath("//button[@aria-label='Log in']"));
+        logIn2.click();
+
+        WebElement popUp=driver.findElement(By.cssSelector(".hAkP2"));//a0A37 hAkP2
+        System.out.println(popUp.getText());
+
+
 
 
     }
